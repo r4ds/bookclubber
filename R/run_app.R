3c2a771a70b9a93ebf386f40dee4c87ea32cc933
Jon Harmon <jonthegeek@gmail.com>
2022-10-17 07:58:31 -0500

    No longer need to ever worry about cookies.

--- R/run_app.R
+++ R/run_app.R
@@ -6,7 +6,7 @@
 #' @export
 run_app <- function(...) {
   app <- shinyslack::shinyslack_app(
-    ui = cookies::add_cookie_handlers(.app_ui),
+    ui = .app_ui,
     server = .app_server,
     team_id = .team_id,
     site_url = .site_url,
