5b9721f93bf705ba76c36d825966c183c4657a33
Jon Harmon <jonthegeek@gmail.com>
2023-10-14 15:35:24 -0500

    Refactor run.

--- R/run.R
+++ R/run.R
@@ -5,16 +5,18 @@
 #'
 #' @export
 run_app <- function(...) {
-  app <- shinyslack::shinyslack_app(
+  golem::with_golem_options(
+    app = .bookclub_app(),
+    golem_opts = list(...)
+  )
+}
+
+.bookclub_app <- function() {
+  shinyslack::shinyslack_app(
     ui = .app_ui,
     server = .app_server,
     team_id = .team_id,
     enableBookmarking = "url",
     onStart = .app_global
-  )
-
-  golem::with_golem_options(
-    app = app,
-    golem_opts = list(...)
   )
 }
