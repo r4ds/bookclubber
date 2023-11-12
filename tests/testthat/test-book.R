e8c1aef3c0189759ef3d019f5a74bd1225502b05
Jon Harmon <jonthegeek@gmail.com>
2023-11-12 13:40:14 -0600

    Uncomplicate book selector. (#113)
    And fix bug when UTC week switches over.

--- tests/testthat/test-book.R
+++ tests/testthat/test-book.R
@@ -3,14 +3,17 @@ test_that("Book UI has expectd form", {
 })
 
 test_that("Book server returns selected book", {
-  testServer(.book_server, {
-    session$setInputs(selected_book = "R for Data Science")
-    book <- session$getReturned()
-    expect_identical(
-      book(),
-      "R for Data Science"
-    )
-  })
+  expect_warning(
+    testServer(.book_server, {
+      session$setInputs(selected_book = "R for Data Science")
+      book <- session$getReturned()
+      expect_identical(
+        book(),
+        "R for Data Science"
+      )
+    }),
+    "MockShinySession"
+  )
 })
 
 # As far as I can find through manual testing, shinytest2 can't "see" query
